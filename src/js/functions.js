c8096ee480342a47c3867eb43868278a7f0de3f2
Thomas Claireau <thomas.claireau@gmail.com>
2019-09-16 07:12:06 +0200

    codacy -> A function with a name starting with an uppercase letter should only be used as a constructor

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,9 +1,9 @@
-import SVGInjector from 'svg-injector';
+import svgInjector from 'svg-injector';
 
 export default {
 	// afficher les svg une fois la page chargée
 	injectSvg: () => {
 		const svgs = document.querySelectorAll('img.js-inject-me');
-		SVGInjector(svgs);
+		svgInjector(svgs);
 	},
 };
